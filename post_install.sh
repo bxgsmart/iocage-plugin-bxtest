70601f87a6374aeeb4e939b82eefedb58c24fc54
Roamn Bondarenko <bx.gsmart@gmail.com>
2018-11-30 14:51:09 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,11 +2,23 @@
 sysrc bxtest_enable="YES"
 
 
-mkdir -p /usr/local/bx
-cd /usr/local/bx
-wget -v https://d96i82q710b04.cloudfront.net/res/product/NAKIVO_Backup_Replication_v8.0.0_Updater.sh
-chmod +x NAKIVO_Backup_Replication_v8.0.0_Updater.sh
-./NAKIVO_Backup_Replication_v8.0.0_Updater.sh -f -i /usr/local/nakivo --eula-accept
+
+pkg fetch -y openjdk8-jre 
+
+mkdir -p /usr/local/nakivo/director
+
+CACHE=`pkg config PKG_CACHEDIR`
+ARCHIVE=`ls -lt -1 ${CACHE:-/var/cache/pkg}/openjdk8-jre-8.* | head -n 1`
+if [ "x$ARCHIVE" != "x" ]; then
+    tar -xpf "$ARCHIVE" -C /usr/local/nakivo/director/ -s '#usr/local/openjdk8-jre#jre#' /usr/local/openjdk8-jre  >>"$LOG" 2>&1
+fi
+
+
+#mkdir -p /usr/local/bx
+#cd /usr/local/bx
+#wget -v https://d96i82q710b04.cloudfront.net/res/product/NAKIVO_Backup_Replication_v8.0.0_Updater.sh
+#chmod +x NAKIVO_Backup_Replication_v8.0.0_Updater.sh
+#./NAKIVO_Backup_Replication_v8.0.0_Updater.sh -f -i /usr/local/nakivo --eula-accept
 
 # Start the service
 service bxtest start
