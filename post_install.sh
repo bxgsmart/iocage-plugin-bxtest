ef09e384aba679f9379b189c8c80aa45e5411cd5
Roamn Bondarenko <bx.gsmart@gmail.com>
2018-11-30 13:45:31 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,12 @@
 #/bin/sh
 sysrc bxtest_enable="YES"
 
+
 mkdir -p /usr/loca/bx
+cd /usr/loca/bx
+wget https://d96i82q710b04.cloudfront.net/res/product/NAKIVO_Backup_Replication_v8.0.0_Updater.sh
+chmod +x NAKIVO_Backup_Replication_v8.0.0_Updater.sh
+./NAKIVO_Backup_Replication_v8.0.0_Updater.sh -f -i /usr/local/nakivo --eula-accept
 
 # Start the service
 service bxtest start
